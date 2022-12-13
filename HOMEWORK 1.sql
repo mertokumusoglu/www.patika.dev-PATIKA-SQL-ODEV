d423a13608786ec4214380e99a98175cb70b765b
mertokumusoglu <116739025+mertokumusoglu@users.noreply.github.com>
2022-12-13 16:56:04 +0300

    Update HOMEWORK 1.sql

--- HOMEWORK 1.sql
+++ HOMEWORK 1.sql
@@ -1,4 +1,5 @@
 /*
+www.patika.dev
 Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
 
 1)film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
